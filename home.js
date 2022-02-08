087980f320314f353fe727d5d7ef84836b9bd558
simran-022 <simransgupta22@gmail.com>
2022-02-08 13:32:10 +0530

    submit

---
+++ home.js
@@ -0,0 +1,7 @@
+function imgSlider(anything){
+    document.querySelector('.img').src = anything;
+}
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
